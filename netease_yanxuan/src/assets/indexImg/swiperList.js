0cc6926e624572969d90549276059e907a095b73
zhouyangyong <3108255033@qq.com>
2019-07-14 22:33:58 +0800

    netease_yanxuan

---
+++ netease_yanxuan/src/assets/indexImg/swiperList.js
@@ -0,0 +1,10 @@
+export const swiperList = [
+  'https://yanxuan.nosdn.127.net/2f9c81a130447f34424a16a40d66cbba.jpg?imageView&quality=75&thumbnail=750x0',
+  'https://yanxuan.nosdn.127.net/3cf97a99f8017e3bb0d22fdff2c283b0.jpg?imageView&amp;quality=75&amp;thumbnail=750x0',
+  'https://yanxuan.nosdn.127.net/978d3c4254a79b7be5ba86a15b0a8557.jpg?imageView&amp;quality=75&amp;thumbnail=750x0',
+  'https://yanxuan.nosdn.127.net/e1838f8442e7bfef014070ff902a698f.jpg?imageView&amp;quality=75&amp;thumbnail=750x0',
+  'https://yanxuan.nosdn.127.net/2f20bfbccb5bcde8fdf2dbd58278ddaa.jpg?imageView&amp;quality=75&amp;thumbnail=750x0',
+  'https://yanxuan.nosdn.127.net/5f2b33b3ed8fccb2ecf07bafbd7032f1.jpg?imageView&amp;quality=75&amp;thumbnail=750x0',
+  'https://yanxuan.nosdn.127.net/7181023f340264dd753c41c3a2df28f8.jpg?imageView&amp;quality=75&amp;thumbnail=750x0',
+  'https://yanxuan.nosdn.127.net/d4c30559a2a681b1c0d5a560f9c99f59.png?imageView&amp;quality=75&amp;thumbnail=750x0'
+]
--- netease_yanxuan/src/components/index/Recommend.jsx
+++ netease_yanxuan/src/components/index/Recommend.jsx
@@ -1,10 +1,48 @@
 import React, { Component } from 'react';
+import Swiper from 'swiper/dist/js/swiper.js';
+import 'swiper/dist/css/swiper.min.css';
+import { swiperList } from '../../assets/indexImg/swiperList';
 
 class Recommend extends Component {
   state = {}
+  componentDidMount() {
+    var mySwiper = new Swiper('.slider-container', {
+      autoplay: {
+        delay: 2000,
+        stopOnLastSlide: false,
+        disableOnInteraction: true,
+      },
+      loop: true,
+      pagination: {
+        el: '.swiper-pagination',
+      }
+    });
+  }
+  renderSwiperItem = () => {
+    return (
+      <>
+        {
+          swiperList.map((item, index) => {
+            return (
+              <div className="swiper-slide" key={index}>
+                <img src={item} width="100%" height="185px" alt="" />
+              </div>
+            )
+          })
+        }
+      </>
+    )
+  }
   render() {
     return (
-      <div>Recommend</div>
+      <div>
+        <div className="slider-container">
+          <div className="swiper-wrapper">
+            {this.renderSwiperItem()}
+          </div>
+          <div className="swiper-pagination"></div>
+        </div>
+      </div>
     );
   }
 }
